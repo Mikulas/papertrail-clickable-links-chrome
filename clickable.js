ba7f0a043bd3103699e089bea1a20d75f60cac4c
Mikuláš Dítě <rullaf@gmail.com>
2016-03-11 18:55:17 +0100

    fix js regex grouping

--- clickable.js
+++ clickable.js
@@ -4,7 +4,7 @@ var replaceTextWithLinks = function (element) {
 	}
 
 	var text = element.textContent;
-	var textWithLinks = text.replace(/https?:\/\/\S+/g, '<a target="_blank" href="$0">$0</a>');
+	var textWithLinks = text.replace(/(https?:\/\/\S+)/g, '<a target="_blank" href="$1">$1</a>');
 	element.innerHTML = textWithLinks;
 	element.classList.add('linkified');
 };
